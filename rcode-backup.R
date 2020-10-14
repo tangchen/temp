f9aca2b39bd7c0d07b5080d04c4e5706461b340f
Chen Tang <tangc@tangc.net>
2020-10-14 11:42:39 -0500

    Create rcode-backup.R

---
+++ rcode-backup.R
@@ -0,0 +1,10 @@
+# colors from the U of I color palettes: https://www.uillinois.edu/OUR/brand/color_palettes
+ui_colors = list(orange = '#E84A27',
+                 blue = '#13294b',
+                 red = '#D50032',
+                 teal = '#0d605e',
+                 grayblue = '#6fafc7',
+                 citron = '#bfd46d',
+                 yellow = '#ffd125',
+                 salmon = '#ee5e5e',
+                 periwinkle = '4f6898')
